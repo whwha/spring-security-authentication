c57e3ea16dbc91f9dfc3ef4204e8db6c225de66d
boorownie <probitanima11@gmail.com>
2024-10-31 22:03:24 +0900

    step3 - filter chain

---
+++ src/main/java/nextstep/app/SecurityConfig.java
@@ -0,0 +1,31 @@
+package nextstep.app;
+
+import nextstep.security.config.DefaultSecurityFilterChain;
+import nextstep.security.config.DelegatingFilterProxy;
+import nextstep.security.config.FilterChainProxy;
+import nextstep.security.config.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+public class SecurityConfig {
+
+    @Bean
+    public DelegatingFilterProxy delegatingFilterProxy() {
+        return new DelegatingFilterProxy(filterChainProxy(List.of(securityFilterChain())));
+    }
+
+    @Bean
+    public FilterChainProxy filterChainProxy(List<SecurityFilterChain> securityFilterChains) {
+        return new FilterChainProxy(securityFilterChains);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain() {
+        return new DefaultSecurityFilterChain(
+                List.of(
+
+                )
+        );
+    }
+}
